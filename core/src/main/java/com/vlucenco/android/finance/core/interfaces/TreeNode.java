4a0a0ce860af52e5e95dcd206bbb8cd6c57e6591
Vitali Lucenco <lucenco.v@gmail.com>
2016-12-04 11:47:23 +0200

    Create TreeNode interface using Composite design pattern

---
+++ core/src/main/java/com/vlucenco/android/finance/core/interfaces/TreeNode.java
@@ -0,0 +1,22 @@
+package com.vlucenco.android.finance.core.interfaces;
+
+import java.util.List;
+
+// Allows creation of tree-set structure for any set of objects which implement TreeNode interface
+// Custom implementation of "Composite" design pattern
+public interface TreeNode {
+
+    long getId();
+
+    void add(TreeNode child);
+
+    void remove(TreeNode child);
+
+    List<TreeNode> getChildren();
+
+    TreeNode getChild(long id);
+
+    TreeNode getParent();
+
+    void setParent(TreeNode parent);
+}
